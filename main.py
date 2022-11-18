cd753b52d1d2bf699c7c60a83401cfecd34aff53
vlad-vifel <vlad.vifel@yandex.ru>
2022-11-18 20:42:07 +0300

    second commit (game with no assets)

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ while True:
             pygame.quit()
             sys.exit()
 
-        screen.fill('black')
-        level.run()
+    screen.fill('black')
+    level.run()
 
-        pygame.display.update()
-        clock.tick()
+    pygame.display.update()
+    clock.tick(60)
--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player(pygame.sprite.Sprite):
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft = pos)
         self.direction = pygame.math.Vector2(0, 0)
-        self.speed = 8
+        self.speed = 1
         self.gravity = 0.8
         self.jump_speed = -16
 
@@ -22,14 +22,16 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+        if keys[pygame.K_UP]:
+            self.jump()
+
 
     def apply_gravity(self):
         self.direction.y += self.gravity
         self.rect.y += self.direction.y
 
+    def jump(self):
+        self.direction.y = self.jump_speed
+
     def update(self):
         self.get_input()
-        self.rect.x += self.direction.x * self.speed
-        print(self.direction.x * self.speed)
-        print('!!!')
-        #self.apply_gravity()
