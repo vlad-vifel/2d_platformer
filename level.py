bf4c476fe84fc7b09d0801ed00e82a33124a57b2
sev4enko <sev.shapkin2003@yandex.ru>
2022-11-24 01:47:36 +0300

    (game without normal jump and without normal size character)

--- level.py
+++ level.py
@@ -61,11 +61,16 @@ class Level:
                 if player.direction.y > 0:
                     player.rect.bottom = sprite.rect.top
                     player.direction.y = 0
-                    # self.on_ground = True
+                    player.on_ground = True
                 elif player.direction.y < 0:
                     player.rect.top = sprite.rect.bottom
                     player.direction.y = 0
-                    # self.on_ground = False
+                    player.on_ceiling = True
+        if player.on_ground and player.direction.y < 0 or player.direction.y > 1:
+            player.on_ground = False
+        if player.on_ceiling and player.direction.y > 0:
+            player.on_ceiling = False
+
 
     def on_ground(self):
         hits = pygame.sprite.spritecollide(self.player.sprite, self.tiles, False)
